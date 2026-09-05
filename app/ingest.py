3683cc46227a8377a7600171d6769404e022f90a
Sabyasachi Banerjee <banerjeesabyasachi375@gmail.com>
2026-09-05 21:54:50 +0530

    removed unused imports

--- app/ingest.py
+++ app/ingest.py
@@ -7,7 +7,6 @@ from langchain_community.document_loaders import (
     CSVLoader,
 )
 
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_google_genai import (
